2ceab3f6025c2daa717617d36e36a168c1bbfc46
benpapac <ben.papac@gmail.com>
2022-10-01 11:01:39 -0700

    add api/testimonials form

--- src/Components/Api/ApiTestimonial.jsx
+++ src/Components/Api/ApiTestimonial.jsx
@@ -1,5 +1,7 @@
 
+import { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
+import TestimonialApiForm from './TestimonialApiForm';
 
     const TESTIMONIALS_QUERY = gql`
         {
@@ -13,22 +15,48 @@ import { useQuery, gql } from '@apollo/client';
     `;
 
 const ApiTestimonial = () => {
-    const data = useQuery(TESTIMONIALS_QUERY).data;
+    const [editing, setEditing] = useState(false);
+     const [testimonialId, setTestimonialId] = useState('');
+
+    const [message, setMessage] = useState("Update Testimonial");
+    const queryData = useQuery(TESTIMONIALS_QUERY).data;
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        await setTestimonialId(e.target.id);
+
+        if(!editing){
+            setEditing(true);
+            setMessage("Cancel");
+
+        
+        } else {
+            setEditing(false);
+            setMessage("Update Testimonial");
+        }
+    }
 
     return (
         <>
-        {data && (
+        {editing 
+        ? (
+            <TestimonialApiForm testimonialId={testimonialId} handleClick={handleClick} message={message} />
+        )
+        : queryData && (
             <>
-            {data.testimonials.map(testimonial => (
+            {queryData.testimonials.map(testimonial => (
                 <>
                 <h3>{testimonial.name}</h3>
                 <h3>{testimonial.occupation} </h3>
                 <h4>Testimonial</h4>
                 <p>{testimonial.testimonial}</p>
+                <button id={testimonial.id} onClick={handleClick}>{message}</button>
                 </>
             ))}
             </>
-        )}
+        )
+        }
         </>
         
     );
